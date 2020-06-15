058ca108121b90b43fa191b93a6c23be12b35962
Mateus Davi Simon <hlah_@hotmail.com>
2020-06-15 19:09:19 -0300

    Resolvido bug do timer no chrome.

--- public/scripts/timer.js
+++ public/scripts/timer.js
@@ -2,13 +2,14 @@ var total_time = document.currentScript.getAttribute("totalTime");
 let seconds_left = total_time;
 let callback = null;
 
-const alarmAudio = new Audio("/sounds/cooking.mp3");
+let alarmAudio = new Audio("/sounds/timer-alarm.mp3");
 
 printTime(seconds_left);
 
 function stopTimer() {
   document.getElementById("timer-display").classList.remove('timer-ended');
   alarmAudio.pause();
+  alarmAudio.currentTime = 0;
   document.getElementById("timer-toggler").innerHTML = "Iniciar";
   clearInterval(callback);
   callback = null;
@@ -21,7 +22,6 @@ function callbackFun() {
     document.getElementById("timer-display").classList.add('timer-ended');
     document.getElementById("timer-toggler").classList.add('toggler-hidden');
     alarmAudio.play();
-    alarmAudio.fastSeek(0);
     clearInterval(callback);
   }
 }
